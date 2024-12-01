7f22e7d81e26e4fa326632a917961a9f4a8df7df
Pete Davis <hello@petedavis.dev>
2024-12-01 22:33:53 +0000

    ad languages to config

---
+++ app.config.js
@@ -0,0 +1,88 @@
+const APP_VARIANTS = [
+	{
+		env: 'development',
+		id: 'dev.petedavis.fithacker.development',
+		name: 'Fithacker (dev)',
+	},
+	{
+		env: 'preview',
+		id: 'dev.petedavis.fithacker.preview',
+		name: 'Fithacker (preview)',
+	},
+	{
+		env: 'production',
+		id: 'dev.petedavis.fithacker',
+		name: 'Fithacker',
+	},
+];
+
+const variant = APP_VARIANTS.find(
+	(variant) => variant.env === process.env.APP_VARIANT
+);
+const bundleIdentifier = variant?.id;
+const name = variant?.name;
+
+export default {
+	expo: {
+		name: name || 'Fithacker',
+		slug: 'fithacker-app',
+		version: '1.0.0',
+		orientation: 'portrait',
+		icon: './assets/images/icon.png',
+		newArchEnabled: true,
+		scheme: 'myapp',
+		userInterfaceStyle: 'automatic',
+		splash: {
+			image: './assets/images/splash.png',
+			resizeMode: 'contain',
+			backgroundColor: '#111122',
+		},
+		ios: {
+			supportsTablet: true,
+			bundleIdentifier: bundleIdentifier || 'dev.petedavis.fithacker',
+			infoPlist: {
+				CFBundleAllowMixedLocalizations: true,
+				CFBundleLocalizations: [
+					'de',
+					'en',
+					'es',
+					'es-ES',
+					'es-MX',
+					'fr',
+					'fr-CA',
+					'fr-FR',
+					'it',
+					'ja',
+					'ko',
+					'pt',
+					'pt-BR',
+					'pt-PT',
+					'zh',
+					'zh-CN',
+				],
+				CFBundleDevelopmentRegion: 'en',
+			},
+		},
+		android: {
+			adaptiveIcon: {
+				foregroundImage: './assets/images/adaptive-icon.png',
+				backgroundColor: '#111122',
+			},
+			package: bundleIdentifier || 'dev.petedavis.fithacker',
+		},
+		web: {
+			bundler: 'metro',
+			output: 'static',
+			favicon: './assets/images/favicon.png',
+		},
+		plugins: ['expo-router', 'expo-localization', 'expo-font'],
+		experiments: {
+			typedRoutes: true,
+		},
+		extra: {
+			eas: {
+				projectId: '6c3e19dc-162f-45b6-98f4-716ce558cd0e',
+			},
+		},
+	},
+};
